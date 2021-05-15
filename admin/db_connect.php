2c05f78b6099760409f6270efe8177a98941ea50
Fahim Uddin <hellofahimuddin@gmail.com>
2021-05-16 02:59:40 +0600

    Upadted db_connect for hosting

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','alumni_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','nsu_channel')or die("Could not connect to mysql".mysqli_error($con));
